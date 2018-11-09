a85bc8e447d3c00f000165889f2cebb36b6e1a7b
Fridolin Pokorny <fridolin@redhat.com>
2018-11-10 00:30:07 +0100

    Query graphql

--- api.py
+++ api.py
@@ -1,2 +1,53 @@
-def a():
-    return {'hello': 'translate'}
+import requests
+
+
+_QUERY = \
+"""
+{
+  cqp(query: "%s") {
+    meanings {
+      queries {
+        text
+        tokens {
+          text
+          bestLemma {
+            lemma
+            morphology {
+              tagStr
+            }
+          }
+          languages {
+            language
+            probability
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+def _analyze(graph_response: dict) -> dict:
+    # graph_response['data']['meanings'][0]['queries'][0]['tokens'][0]['text']
+    return {"hello": "there!"}
+
+
+def translate(text: str) -> dict:
+    response = raw_graphql_query(text)
+    return _analyze(response)
+
+
+def raw_graphql_query(text: str) -> dict:
+    query = _QUERY % text
+    response = requests.post(
+      'https://cqc.seznam.net/hackathon/graphql',
+      headers={
+        "Authorization": "Basic aGFja2F0aG9uOkFoSjR4aWU2bGllME9wYXU=",
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      json={"query": query}
+    )
+
+    return response.json()
