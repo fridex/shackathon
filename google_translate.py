8fe3a2ffd864c0a0f894a6885adea090ac050593
Fridolin Pokorny <fridolin@redhat.com>
2018-11-10 11:27:46 +0100

    Fix api

--- google_translate.py
+++ google_translate.py
@@ -35,6 +35,16 @@ def iter_chunks(data: list):
 
 
 def translate(query: str, from_lang='cs', to_lang='en') -> list:
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(BASE_URL)
+
+    driver.implicitly_wait(0.25)
+
     lang_mod = f"#{from_lang}/{to_lang}/"
     quoted_query = lang_mod + parse.quote(query)
     
@@ -53,14 +63,3 @@ def swap_languages():
 
     swap_button = driver.find_element_by_id('gt-swap')
     swap_button.click()
-
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(BASE_URL)
-
-driver.implicitly_wait(0.25)
