18d33c67efce99a02fdacec2cf75afb8ab4fca71
Marek Cermak <macermak@redhat.com>
2018-11-10 03:33:57 +0100

    Modify data/
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   api.py
    modified:   data/src-test-cz.txt
    modified:   data/src-val-cz.txt

--- api.py
+++ api.py
@@ -1,5 +1,11 @@
 import requests
 
+from urllib import parse
+from xmlrpc.client import ServerProxy
+
+
+BASE_URL  = "https://slovnik.seznam.cz/{from_lang}-{to_lang}/?q={query}"
+QUERY_URL = "https://api.slovnik.seznam.cz/rpc2"
 
 _QUERY = \
 """
@@ -28,13 +34,38 @@ _QUERY = \
 """
 
 
-def _analyze(graph_response: dict) -> dict:
+client = ServerProxy(QUERY_URL)
+
+
+def _should_anlayze(graph_response) -> bool:
+
+
+
+def _translate(graph_response: dict) -> dict:
     # graph_response['data']['meanings'][0]['queries'][0]['tokens'][0]['text']
-    return {"hello": "there!"}
+
+    if _should_analyze(graph_response):
+        translation = client.toolbar.search(query, f"{from_lang}_{to_lang}")
+
+    else:
+        translation = {'status': 400, 'statusMessage': 'Bad Request', 'translations': []}
+
+    return translation
+
+
+def _construct_link(query: str, from_lang='cz', to_lang='en') -> str:
+    link = BASE_URL.format(
+        from_lang=from_lang,
+        to_lang=to_lang,
+        query=parse.quote(query)
+    )
+
+    return link
 
 
 def translate(text: str) -> dict:
     response = raw_graphql_query(text)
+
     return _analyze(response)
 
 
