901620539589856a689f3cd1b717e9883d243a6c
Marek Cermak <macermak@redhat.com>
2018-11-10 08:40:07 +0100

    Translation acceptance logic
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   api.py

--- api.py
+++ api.py
@@ -5,9 +5,13 @@ import requests
 
 from google_translate import translate
 
-
 BASE_URL  = "https://slovnik.seznam.cz/{source_lang}-{target_lang}/?q={query}"
 QUERY_URL = "https://api.slovnik.seznam.cz/rpc2"
+
+ALLOWED_COMMANDS = ["přeložit", "překlad", "jak se říct"]
+ALLOWED_LANGUAGE_SPEC  = ["anglicky", "anglický", "angličtina"]
+ALLOWED_EXACTS = ["anglicky", "v anglictine", "v angličtině"]
+
 
 _QUERY = \
 """
@@ -39,9 +43,58 @@ _QUERY = \
 client = ServerProxy(QUERY_URL)
 
 
-def _should_analyze(graph_response) -> bool:
+def _get_parsed_attrs(graph_response: dict) -> list:
+    meaning, = response['data']['cqp']['meanings']
+    meaning
 
-    return True
+    tokens =  meaning['queries'][0]['tokens']
+
+    lemmas = []
+    correction = []
+
+    for token_attr in tokens:
+        text = token_attr['text']
+        best_lemma = token_attr.get('bestLemma', None)
+
+        if best_lemma:
+            lemmas.append(best_lemma['lemma'])
+
+            correction.append(text)
+
+    return correction, lemmas
+
+
+def _should_analyze(graph_response: dict) -> bool:
+
+    if graph_response:
+
+        correction, lemmas = _get_parsed_attrs(response)
+
+        # matches checks
+        cmd_allowed = False
+        for cmd in allowed_commands:
+            query_cmd = " ".join(lemmas[:len(cmd.split(" "))])
+            cmd_allowed = query_cmd  == cmd
+
+            if cmd_allowed:
+                break
+
+        valid_lang_spec = lemmas[-1] in allowed_language_spec
+
+        exact_allowed = False
+        for exact in ALLOWED_EXACTS:
+            query_exact = " ".join(correction[-len(exact.split(" ")):])
+
+            exact_allowed = query_exact == exact
+
+            if exact_allowed:
+                break
+
+
+        if exact_allowed or (cmd_allowed and valid_lang_spec):
+            return True
+
+    return False
 
 
 def _translate(graph_response: dict,
