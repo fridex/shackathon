7bf2a4e8c16fac01ae6a6ce4343eab0afe775e1b
Fridolin Pokorny <fridolin@redhat.com>
2018-11-10 04:59:35 +0100

    Serve static content

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,19 @@
+import os
 import connexion
+from flask import Flask, request, send_from_directory
 
 app = connexion.App(__name__)
 application = app.app
 app.add_api('swagger.yaml')
 
-def a():
-    return {'hello': 'translate'}
+
+@app.route('/')
+def index():
+    print('index')
+    return send_from_directory('hackathon-frontend', 'index.html')
+
+
+@app.route('/<path:path>')
+def assets(path):
+    print(path)
+    return send_from_directory(os.path.join('hackathon-frontend', 'assets'), path)
