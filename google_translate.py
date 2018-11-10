aa50e692ba5fc595de81de7d19ee7e182dbd0833
Fridolin Pokorny <fridolin@redhat.com>
2018-11-10 11:24:39 +0100

    Fix everything

--- google_translate.py
+++ google_translate.py
@@ -57,6 +57,8 @@ def swap_languages():
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(BASE_URL)
